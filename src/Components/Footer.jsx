af8811093a03ccdea30515fc727497b7f4518d0b
oscardokis <oscar-gonzalezd1@hotmail.com>
2024-04-05 08:28:02 -0500

    adding hr home margin

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
   return (
     <div className="flex flex-col items-center text-white p-3 text-xs w-full">
       <hr className="h-[2px] w-full min-w-[18rem] border-t-0 bg-transparent bg-gradient-to-r from-transparent via-bluelight to-transparent bg-center md:my-3" />
-      <h2 className="mb-3 text-bluelight">Design by Oscar Gonzalez © {year} </h2>
+      <h2 className="mb-3 text-bluelight mt-3">Design by Oscar Gonzalez © {year} </h2>
     </div>
   )
 }
